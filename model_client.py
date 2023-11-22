26429ab3dfa62f6d793e62c1c1ab1be8dc6b2353
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2023-11-22 07:00:41 -0600

    Update model_client.py

--- model_client.py
+++ model_client.py
@@ -11,13 +11,14 @@ def make_prediction(hours_worked):
     prediction = response.json()
     return prediction['predictions'][0][0]
 
-def calculate_efficiency(hours_worked):
-    # Usar la fórmula Y=3X+2 para calcular una métrica relacionada con la eficiencia del programador
-    efficiency_metric = 3 * hours_worked + 2
-    return efficiency_metric
+def calculate_deficiency(hours_worked):
+    # Usar la fórmula Y=3X+2 para calcular una métrica relacionada con la deficiencia del programador
+    target_hours = 40  # Establecer las horas de trabajo establecidas
+    deficiency_metric = max(0, hours_worked - target_hours)
+    return deficiency_metric
 
 def main():
-    st.title('Calculadora de Eficiencia del Programador')
+    st.title('Calculadora de Deficiencia del Programador')
 
     hours_worked = st.number_input('Ingrese el número de horas trabajadas:', min_value=0.0, step=1.0)
 
@@ -25,10 +26,10 @@ def main():
         # Hacer la llamada al servidor externo para predecir la métrica
         predicted_efficiency = make_prediction(hours_worked)
         
-        # Calcular la métrica relacionada con la eficiencia del programador
-        efficiency_result = calculate_efficiency(hours_worked)
+        # Calcular la métrica relacionada con la deficiencia del programador
+        deficiency_result = calculate_deficiency(hours_worked)
         
-        st.write(f'Métrica de eficiencia del programador para {hours_worked} horas trabajadas: {efficiency_result}')
+        st.write(f'Deficiencia del programador por exceder las horas establecidas ({hours_worked} horas trabajadas): {deficiency_result}')
         st.write(f'Métrica predicha por el servidor externo: {predicted_efficiency}')
 
 if __name__ == '__main__':
